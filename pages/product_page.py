84f077be23d773d67c7c3c1c03a22f38a5ca2530
O-Yama-absinthe <mrabsinthe834@gmail.com>
2021-02-02 23:28:55 +0300

    added unsuccess checkings(test_guest_cant_see_success_message_after_adding_product_to_basket, test_guest_cant_see_success_message, test_message_disappeared_after_adding_product_to_basket)

--- pages/product_page.py
+++ pages/product_page.py
@@ -4,11 +4,20 @@ import time
 
 class ProductPage(BasePage):
  def add_button_actions(self):
-  name=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
   assert self.is_element_present(*ProductPageLocators.BASKET_BUTTON), "Basket-button is not presented"
   basket=self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
   basket.click()
   self.solve_quiz_and_get_code()
-
-  added=self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADDING_MESSAGE).text
-  assert name==added, "Product name is not matching product name after adding to basket"
+ 
+ def names_matched(self):
+  assert self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text==self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_ADDING_MESSAGE).text, \
+ "Product name is not matching product name after adding to basket"
+ 
+ def cant_see_success_message_after_adding_product_to_basket(self):
+  assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_ADDING_MESSAGE), "Success message is presented, but should not be"
+ 
+ def cant_see_success_message(self):
+  assert self.is_not_element_present(*ProductPageLocators.PRODUCT_NAME_ADDING_MESSAGE), "Success message is presented, but should not be"
+  
+ def message_disappeared_after_adding_product_to_basket(self):
+  assert self.is_disappeared(*ProductPageLocators.PRODUCT_NAME_ADDING_MESSAGE), "Success message is disappeared after adding to basket"
--- test_product_page.py
+++ test_product_page.py
@@ -18,5 +18,30 @@ def test_guest_can_add_product_to_basket(browser,link):
  #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
  page=ProductPage(browser,link)
  page.open()
- page.add_button_actions()
  time.sleep(3)
+ page.add_button_actions()
+ page.names_matched
+ 
+@pytest.mark.xfail
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser): 
+  link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+  page=ProductPage(browser,link)
+  page.open()
+  page.add_button_actions()
+  time.sleep(3)
+  page.cant_see_success_message_after_adding_product_to_basket()
+  
+@pytest.mark.xfail
+def test_guest_cant_see_success_message(browser): 
+  link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+  page=ProductPage(browser,link)
+  page.open()
+  page.cant_see_success_message()
+  
+@pytest.mark.three
+def test_message_disappeared_after_adding_product_to_basket(browser):
+  link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+  page=ProductPage(browser,link)
+  page.open()
+  page.add_button_actions()
+  page.message_disappeared_after_adding_product_to_basket()
